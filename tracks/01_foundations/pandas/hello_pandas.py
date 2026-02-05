935db0de9ebd1e743a50d799667ad5daffa1f309
Akira <vitorucha96@gmail.com>
2026-02-05 14:27:49 -0300

    chore: remove unused folders

--- tracks/01_foundations/pandas/hello_pandas.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.DataFrame({"nome": ["Akira", "Chat"], "nota": [10, 10]})
-print(df)
-print("\nDescribe:")
-print(df.describe(include="all"))
--- tracks/01_foundations/python_fundamentos/try1.py
+++
@@ -1,5 +0,0 @@
-try:
-    age = int(input("Type your age: "))
-    print(f"You typed: {age}")
-except:
-    print("Invalid number. Please type only digits.")
